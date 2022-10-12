47d435d7003bca4cc93b185ff986dd03e9375269
Jesus Martin Barajas Coronado <jesus.barajas@coppel.com>
2022-10-11 20:53:16 -0600

    removing unused pages

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,12 +1,16 @@
 import { Flex, Box } from "@chakra-ui/react";
 import Dashboard from "../dashboard/Dashboard";
 import { userProfile, userOptions } from "../../fixtures/user";
+import Head from "next/head";
 
 export default function Layout(props) {
 	const { data: userData } = userProfile;
 
 	return (
 		<Flex h="100vh" flexDir="row" overflow="hidden" maxW="2000px">
+			<Head>
+				<title>Kraken</title>
+			</Head>
 			<Dashboard userProfile={userData} userOptions={userOptions.data} />
 			<>{props.children}</>
 		</Flex>
