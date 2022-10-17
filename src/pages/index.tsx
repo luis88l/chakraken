31d6cc92ec486c7b0c3ea8fd83a4e8702d4d6373
gosuna <gosuna@eboxcode.com>
2022-10-17 15:05:12 -0600

    se crea archivo general para importar componentes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,10 @@ import {
 } from "@chakra-ui/react";
 import { FiLock, FiUser, FiArrowRight, FiEye, FiEyeOff } from "react-icons/fi";
 import { useRouter } from "next/router";
-
 import { useState, useEffect } from "react";
 import Head from "next/head";
 import { userLogin } from "../fixtures/login";
-import KAlert from "../components/alert/KAlert";
+import { KAlert } from "../components/react";
 
 const Index = () => {
 	const router = useRouter();
