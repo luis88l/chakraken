64bfca44cb9b3d97004bf4adafb5271f64185a9a
mrtnbrjs <jesus.martin2093@gmail.com>
2023-01-19 22:33:49 -0700

    merge con todo

--- src/pages/dashboard/afore/index.tsx
+++ src/pages/dashboard/afore/index.tsx
@@ -1,17 +1,14 @@
 import { getSession } from "next-auth/react";
 import KPage from "../../../components/page/KPage";
-import { Box, Button, ButtonGroup, Checkbox, Text } from "@chakra-ui/react";
+import { Box, Button, ButtonGroup, Checkbox } from "@chakra-ui/react";
 import Link from "next/link";
-import { useMutation, useQuery } from "react-query";
+import { useQuery } from "react-query";
 import ApiService from "../../../../data/services/ApiService";
-import { createColumnHelper, Pagination } from "@tanstack/react-table";
-import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import { createColumnHelper } from "@tanstack/react-table";
+import { DeleteIcon } from "@chakra-ui/icons";
 import { KTableLayout } from "../../../components/tableLayout/KTableLayout";
 import KSkeletonPage from "../../../components/skeleton/KSkeletonPage";
-import router from "next/router";
-import { render } from "react-dom";
 import React, { useState } from "react";
-import { Props } from "next/script";
 
 export interface aforeTable {
   id: string;
@@ -37,11 +34,7 @@ export default function Afore(): any {
     isLoading,
     data: aforeModulo,
     isSuccess,
-<<<<<<< HEAD
   } = useQuery("Afore", async () => await ApiService.aforeGet(true));
-=======
-  } = useQuery("afore", async () => ApiService);
->>>>>>> origin/product-feed
 
   if (isLoading) {
     return <KSkeletonPage />;
@@ -50,8 +43,10 @@ export default function Afore(): any {
   const aforeData = aforeModulo.data.data;
   const columnHelper = createColumnHelper<aforeTable>();
 
-  const handleClick = async (event: { preventDefault: () => void }) => {
-    ApiService.aforeGet(true).then((response) => {
+  const handleClick = async (event: {
+    preventDefault: () => void;
+  }): Promise<any> => {
+    void ApiService.aforeGet(true).then((response) => {
       const datos = response.data.data;
       setafo(datos);
     });
@@ -98,72 +93,67 @@ export default function Afore(): any {
     }),
   ];
 
-<<<<<<< HEAD
   if (isSuccess) {
     return (
       <KPage title="Afore">
-        <Box ml={"52"} mb="5" mt={3}>
-=======
-  return (
-    <KPage title="Afore">
-      <Box m={5} mt="10" overflow="scroll" max-height="100%" width="100%">
-        <Box ml={"56"} mb="10">
->>>>>>> origin/product-feed
-          <Button
-            mr={"5"}
-            alignItems={"center"}
-            size="md"
-            bg="blue.500"
-            textColor={"white"}
-            rounded="5"
-          >
-            Marcar enviados
-          </Button>
-          <Button
-            rounded="5"
-            size={"md"}
-            bg="blue.500"
-            textColor={"white"}
-            alignItems="center"
-          >
-            Descargar Csv
-          </Button>
-          <Button
-            onClick={handleClick}
-            rounded="5"
-            ml={"5"}
-            size={"md"}
-            bg="blue.500"
-            textColor={"white"}
-            alignItems="center"
-          >
-            Actualizar lista
-          </Button>
-          <Checkbox
-            alignContent={"center"}
-            size="lg"
-            ml={5}
-            mt={1.5}
-            spacing={"3"}
-          >
-            Todos
-          </Checkbox>
-        </Box>
-        <Box bg={"ButtonShadow"} alignItems="center">
-          {Array.isArray(datos) && (
-            <KTableLayout
-              columns={columns}
-              data={datos.map(
-                ({ id, nombre_completo, correo, curp, telefono }) => ({
-                  id,
-                  nombre_completo,
-                  correo,
-                  curp,
-                  telefono,
-                })
-              )}
-            />
-          )}
+        <Box overflow="scroll" max-height="100%" width="100%">
+          <Box ml={"52"} mb="5" mt={3}>
+            <Button
+              mr={"5"}
+              alignItems={"center"}
+              size="md"
+              bg="blue.500"
+              textColor={"white"}
+              rounded="5"
+            >
+              Marcar enviados
+            </Button>
+            <Button
+              rounded="5"
+              size={"md"}
+              bg="blue.500"
+              textColor={"white"}
+              alignItems="center"
+            >
+              Descargar Csv
+            </Button>
+            <Button
+              onClick={handleClick}
+              rounded="5"
+              ml={"5"}
+              size={"md"}
+              bg="blue.500"
+              textColor={"white"}
+              alignItems="center"
+            >
+              Actualizar lista
+            </Button>
+            <Checkbox
+              alignContent={"center"}
+              size="lg"
+              ml={5}
+              mt={1.5}
+              spacing={"3"}
+            >
+              Todos
+            </Checkbox>
+          </Box>
+          <Box bg={"ButtonShadow"} alignItems="center">
+            {Array.isArray(datos) && (
+              <KTableLayout
+                columns={columns}
+                data={datos.map(
+                  ({ id, nombre_completo, correo, curp, telefono }) => ({
+                    id,
+                    nombre_completo,
+                    correo,
+                    curp,
+                    telefono,
+                  })
+                )}
+              />
+            )}
+          </Box>
         </Box>
       </KPage>
     );
