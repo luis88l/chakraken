08321666cfa66b0e38f344e7998aedfc51217ca0
mrtnbrjs <jesus.martin2093@gmail.com>
2023-02-22 07:15:15 -0700

    url sitemaps

--- src/pages/dashboard/seo/sitemaps.tsx
+++ src/pages/dashboard/seo/sitemaps.tsx
@@ -23,7 +23,7 @@ export default function Sitemaps(): any {
     {
       onSuccess: (formData: any) => {
         const filenameString: string = formData.data.filename.toString();
-        const downloadString = "http://localhost:8080/xml/" + filenameString;
+        const downloadString = "http://apikraken/xml/" + filenameString;
         void downloadURI(downloadString, filenameString, "xml");
       },
     }
