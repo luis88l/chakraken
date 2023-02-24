9bc88d0e39dda2b4da74671108608dfa438f2c6c
Jesus Martin Barajas Coronado <jesus.barajas@coppel.com>
2023-02-24 10:41:28 -0700

    cambios login

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -13,7 +13,7 @@ export default function Dashboard(props: DashboardProps): any {
   return (
     <>
       <Flex
-        w={{ base: "100%", lg: "25%", md: "20%", sm: "25%" }}
+        w={{ base: "100%", lg: "18%", md: "18%", sm: "18%" }}
         flexDir="column"
         alignItems="center"
         backgroundColor={{
